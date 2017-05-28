157b51a995476b2b1375d05d61e864eb3a9c91be
Tanzeel Rana <tanzeelrana@cmail.carleton.ca>
2017-05-27 23:56:04 -0400

    Create app.config.ts

---
+++ mobile/app/src/app/app.config.ts
@@ -0,0 +1,45 @@
+import * as AWS from 'aws-sdk';
+import { AlertsService } from '../providers/alerts-service';
+import { ModalService } from '../providers/modal-service';
+
+class AppConfig {
+
+	private logLevel;
+	private osrm;
+
+	constructor(
+
+	) {
+		this.osrm = {
+			"serverURL": "http://osrm.tanzeelrana.me/nominatim/"
+		};
+	}
+
+
+
+
+	public initAWS(){
+		console.log("initAWS");
+		AWS.config.update({
+	        accessKeyId: "YOUR_ACCESS_KEY",
+	        secretAccessKey: "YOUR_SECRET_KEY",
+	        "region": "REGION"
+	    });
+	};
+
+	private setLogLevel(logLevel){
+		this.logLevel = logLevel;
+	}
+
+	public getLogLevel(){
+		return this.logLevel;
+	}
+
+	
+
+	public getOSRMData(){
+		return this.osrm;
+	}
+}
+
+export let config = new AppConfig();
